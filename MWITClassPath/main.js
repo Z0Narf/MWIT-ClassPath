43c484851e821f3219cf7124c05408492d3527e1
Z0Narf <Z0Narf@users.noreply.github.com>
2023-02-24 23:35:57 +0700

    Update main js

--- MWITClassPath/main.js
+++ MWITClassPath/main.js
@@ -1,32 +1,87 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
-import { setupButton } from './page1.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+import './style.css';
+import javascriptLogo from './javascript.svg';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <div className="card">
+        <button id="counter" type="button"></button>
+      </div>
+      <div className="card">
+        <Link to="/page1">Go to page 1</Link>
+      </div>
     </div>
-    <div class="card">
-  <button id="url" type="button">Go to other page</button>
+  );
+}
+
+function Page1() {
+  const url = 'https://youtu.be/62_osPKmIIg';
+
+  return (
+    <div>
+      <h2>Page 1</h2>
+      <div className="card">
+        <button id="url" type="button" onClick={() => window.location.href = url}>Go to YouTube</button>
+      </div>
+      <div className="card">
+        <Link to="/page2">Go to page 2</Link>
+      </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  );
+}
 
-setupCounter(document.querySelector('#counter'))
+function Page2() {
+  const url = 'https://www.example.com/';
 
-const url = 'https://youtu.be/62_osPKmIIg'
-const button = document.querySelector('#url')
-setupButton(button, url)
+  return (
+    <div>
+      <h2>Page 2</h2>
+      <div className="card">
+        <button id="url" type="button" onClick={() => window.location.href = url}>Go to Example</button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page1">Page 1</Link>
+            </li>
+            <li>
+              <Link to="/page2">Page 2</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/page1">
+            <Page1 />
+          </Route>
+          <Route path="/page2">
+            <Page2 />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
+
 
